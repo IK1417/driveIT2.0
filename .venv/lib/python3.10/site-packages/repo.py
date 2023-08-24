8de4b0f492d2607f83600455e8a90d1332c49665
rasput <rasputyashka@gmail.com>
2023-08-24 14:16:27 +0300

    made a package l ike

---
+++ .venv/lib/python3.10/site-packages/repo.py
@@ -0,0 +1,18 @@
+class Repo:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def get_holiday(self, date):
+        cur = self.conn.cursor()
+        records = cur.execute(
+            'SELECT holiday_name FROM holidays WHERE date=?', (date,)
+        )
+        if records:
+            return [x[0] for x in records.fetchall()]
+        else:
+            return []
+
+    def get_stations(self):
+        cur = self.conn.cursor()
+        cur.execute('SELECT station from stations')
+        return [x[0] for x in cur.fetchall()]
