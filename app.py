9ee5bebea6119cc9e0e1414cf1921eedc95e262b
rasput <rasputyashka@gmail.com>
2023-08-24 13:46:46 +0300

    pretested, no model

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask, request
+import sqlite3
 import json
 
-app = Flask(__name__)
+from flask import Flask, request, g, jsonify
 
-# дата, время, станция, погода (в цельсиях)
+from repo import Repo
+from ml_model import predict
 
-@app.route(methods=["GET"])
-def model_request_handler():
-    model_params = json.loads(request.data)
-    
+
+def get_db_connection():
+    conn = sqlite3.connect('db')
+    return conn
+
+def predict_traffic():
+    print(request.data)
+    data = json.loads(request.data)
+    date, time = data['datetime'].split()
+    precipitation = float(data['precipitationMm'])
+    time = time.removesuffix(':00')
+
+    traffic = {}
+
+    repo = g.repo
+    stations = repo.get_stations()
+    holiday_name = repo.get_holiday(date)
+    for station in stations:
+        traffic[station] = predict(date, time, precipitation, holiday_name)
+
+    return jsonify({"stations": traffic}), 200
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.before_request
+    def add_repo():
+        conn = get_db_connection()
+        g.repo = Repo(conn)
+
+    app.add_url_rule('/predict', view_func=predict_traffic, methods=['POST'])
+    return app
+
+def main():
+    app = create_app()
+    app.run()
+
+if __name__ == '__main__':
+    main()
